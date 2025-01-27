8bcb04657d8ccfc0b28f26f8049262cac35e82f4
Joyce Lin <ohanalin@gmail.com>
2025-01-27 11:32:37 -0500

    update

--- js/loadTemplates.js
+++ js/loadTemplates.js
@@ -1,20 +1,4 @@
-// function loadTemplate(templateId, templatePath) {
-//     fetch(templatePath)
-//         .then(response => response.text())
-//         .then(data => {
-//             document.getElementById(templateId).innerHTML = data;
-//             if (templateId === 'nav-placeholder') {
-//                 initializeNavToggle();
-//             }
-//         })
-//         .catch(error => console.error('Error loading template:', error));
-// }
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     loadTemplate('nav-placeholder', 'templates/nav.html');
-//     loadTemplate('footer-placeholder', 'templates/footer.html');
-//     loadTemplate('contactform-placeholder', 'templates/contactform.html');
-// });
 
 function loadTemplate(templateId, templatePath) {
     fetch(templatePath)
@@ -35,7 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
     loadTemplate('contactform-placeholder', '../templates/contactform.html');
 });
 
+function openService(evt, serviceName) {
+    var i, tabcontent, tablinks;
 
+    // Hide all tab content
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the active class from all tab links
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab content and add the active class to the clicked tab link
+    document.getElementById(serviceName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set the default tab to be open
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.tablinks').click();
+});
 
 function initializeNavToggle() {
     const navToggle = document.querySelector("#navToggle");
